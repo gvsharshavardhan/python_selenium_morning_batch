82d4effd18a75d093eb53baa14ad2e6251d39afb
Harsha Vardhan <gvsharsha6316@gmail.com>
2021-04-03 10:32:43 +0530

    updated quiz program with comments

--- quizprog.py
+++ quizprog.py
@@ -2,16 +2,37 @@ from random import randint
 quizdict = {}
 datafromfile = []
 
+"""
+reading data from file and stoting it in a list.
+readlines -> reads data as list of lines(string)
+"""
 with open("E:/python_morning_Sessions/quiz.txt", "r") as f:
     datafromfile = f.readlines()
 
+
+"""
+seperating questions and answers and adding them to quizdict
+"""
 for i in datafromfile:
     line = i.split(":")
     quizdict[line[0]] = line[1][:len(line[1])-1]
 
+"""
+converting dict to list of tuples(questions and answers)
+eg:
+---
+[(q1,a1),(q2,a2),(q3,a3)]
+"""
 quizQandA = list(quizdict.items())
 # i = randint(0, len(q)-1)
 
+
+"""
+looping through list of tuples(questions and answers) , verifying answers and updating score.
+eg:
+---
+[(q1,a1),(q2,a2),(q3,a3)]
+"""
 score = 0
 for qa in quizQandA:
     print(qa[0])
@@ -21,4 +42,6 @@ for qa in quizQandA:
     else:
         score -= 1
 
+
+# printing score
 print("your score is: {}".format(score))
